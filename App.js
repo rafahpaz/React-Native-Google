9652d53781d3518217bbb1f0f45c63b725a23e6f
Rafael Paz <99267530+rafahpaz@users.noreply.github.com>
2023-04-19 09:45:36 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Image
+          source={{ uri: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png' }}
+          style={styles.logo}
+        />
+      </View>
+      <View style={styles.body}>
+        <View style={styles.searchBarContainer}>
+          <TextInput
+            style={styles.searchBarInput}
+            placeholder="Search or type URL"
+            placeholderTextColor="#666"
+          />
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity>
+          <Text style={styles.footerText}>Português (Brasil)</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  header: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 300,
+    height: 100,
+  },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#4285F4',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 4,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  links: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  link: {
+    paddingHorizontal: 5,
+  },
+  linkText: {
+    color: '#666',
+    fontSize: 16,
+  },
+  footer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 20,
+  },
+  footerText: {
+    color: '#666',
+    fontSize: 16,
+  },
+  searchBarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    paddingHorizontal: 60,
+    paddingVertical: 8,
+    marginVertical: 10,
+  },
+  searchBarInput: {
+    flex: 1,
+    marginLeft: 30,
+    color: '#666'
+  }
 });
+
+export default HomeScreen;
